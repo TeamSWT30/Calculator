e27ef57b1cd3e7ca54d67fe7114c9be61f492824
Simon Kroer Ochoa <simonkroer@gmail.com>
2018-02-14 15:34:05 +0100

    wat

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine("{0}",calc.Power(5, 2.1));
+        }
+    }
+}
