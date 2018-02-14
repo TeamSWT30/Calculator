720eecbeaf31d589bcfea9d995246b6ee7794511
Tobias <tobiasniklassen@gmail.com>
2018-02-14 15:09:28 +0100

    added new func

--- Calculator.Test.Unit/CalculatorUnitTests.cs
+++ Calculator.Test.Unit/CalculatorUnitTests.cs
@@ -57,10 +57,94 @@ namespace Calculator.Test.Unit
         [TestCase(1, -10, 1)]
         [TestCase(10, 0, 1)]
         [TestCase(4, 0.5, 2.0)]
-		[TestCase(9, 0.5, 3.0)]
+        [TestCase(9, 0.5, 3.0)]
         public void Power_RaiseNumbers_ResultIsCorrect(double x, double exp, double result)
         {
             Assert.That(_uut.Power(x, exp), Is.EqualTo(result));
         }
+
+        [TestCase(2, 2, 1)]
+        [TestCase(-2, -4, 0.5)]
+        [TestCase(-4, 10, -0.4)]
+        [TestCase(8, -5, -1.6)]
+        public void Divide_DivideNumbers_ResultIsCorrect(double dividend, double divisor, double result)
+        {
+            Assert.That(_uut.Divide(dividend, divisor), Is.EqualTo(result));
+        }
+
+        [TestCase(-5, 0)]
+        [TestCase(2, 0)]
+        public void TestDivideByZeroException(double dividend, double divisor)
+        {
+            Assert.Throws<DivideByZeroException>(() => _uut.Divide(dividend, divisor));
+        }
+
+        [TestCase(-5, 2.1)]
+        [TestCase(-2, 0.5)]
+        public void TestPowerException(double a, double b)
+        {
+            Assert.Throws<InvalidOperationException>(() => _uut.Power(a, b));
+        }
+
+        [Test]
+        public void TestAccumulator()
+        {
+            Assert.That(_uut.Accumulator, Is.EqualTo(0));
+        }
+
+
+        [TestCase(2, 4)]
+        [TestCase(-2, 0)]
+        public void AddOverload_AddPosAndNegNumbers_ResultIsCorrect(int a, int result)
+        {
+            _uut.Add(1, 1);
+            Assert.That(_uut.Add(a), Is.EqualTo(result));
+        }
+
+
+        [TestCase(2, 0)]
+        [TestCase(-2, 4)]
+        [TestCase(5, -3)]
+        public void SubtractOverload_SubtractPosAndNegNumbers_ResultIsCorrect(int a, int result)
+        {
+            _uut.Add(1, 1);
+            Assert.That(_uut.Subtract(a), Is.EqualTo(result));
+        }
+
+
+        //[TestCase(3, 2, 6)]
+        //[TestCase(-3, -2, 6)]
+        //[TestCase(-3, 2, -6)]
+        //[TestCase(3, -2, -6)]
+        //[TestCase(0, -2, 0)]
+        //[TestCase(-2, 0, 0)]
+        //[TestCase(0, 0, 0)]
+        //public void MultiplyOverload_MultiplyNunmbers_ResultIsCorrect(int a, int b, int result)
+        //{
+        //    Assert.That(_uut.Multiply(a, b), Is.EqualTo(result));
+        //}
+
+
+        //[TestCase(2, 3, 8)]
+        //[TestCase(2, -3, 0.125)]
+        //[TestCase(-2, -3, -0.125)]
+        //[TestCase(1, 10, 1)]
+        //[TestCase(1, -10, 1)]
+        //[TestCase(10, 0, 1)]
+        //[TestCase(4, 0.5, 2.0)]
+        //[TestCase(9, 0.5, 3.0)]
+        //public void Power_RaiseNumbers_ResultIsCorrect(double x, double exp, double result)
+        //{
+        //    Assert.That(_uut.Power(x, exp), Is.EqualTo(result));
+        //}
+
+        //[TestCase(2, 2, 1)]
+        //[TestCase(-2, -4, 0.5)]
+        //[TestCase(-4, 10, -0.4)]
+        //[TestCase(8, -5, -1.6)]
+        //public void Divide_DivideNumbers_ResultIsCorrect(double dividend, double divisor, double result)
+        //{
+        //    Assert.That(_uut.Divide(dividend, divisor), Is.EqualTo(result));
+        //}
     }
 }
--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,27 +1,97 @@
 ﻿using System;
+using System.Data;
 
 namespace Calculator
 {
     public class Calculator
     {
+        public double Accumulator { get; private set; } = 0;
+
+        public Calculator()
+        {
+            Accumulator = 0;
+        }
+
         public double Add(double a, double b)
         {
-            return a + b;
+            return Accumulator = a + b;
         }
 
         public double Subtract(double a, double b)
         {
-            return a - b;
+            return Accumulator = a - b;
         }
 
         public double Multiply(double a, double b)
         {
-            return a * b;
+            return Accumulator = a * b;
         }
 
         public double Power(double a, double b)
         {
-            return Math.Pow(a, b);
+            if (a < 0 && b != Convert.ToInt32(b))
+            {
+                Accumulator = 0;
+                throw new InvalidOperationException("Number is complex");
+            }
+                return Accumulator = Math.Pow(a, b);
         }
+
+        public double Divide(double dividend, double divisor)
+        {
+            if (divisor == 0)
+            {
+                Accumulator = 0;
+                throw new DivideByZeroException();
+            }
+            return Accumulator = dividend / divisor;
+        }
+
+        public void Clear()
+        {
+            Accumulator = 0;
+        }
+
+        public double Add(double addend)
+        {
+            return Accumulator = Accumulator + addend;
+        }
+
+        public double Subtract(double subtractor)
+        {
+            return Accumulator = Accumulator - subtractor;
+        }
+
+        public double Multiply(double multiplier)
+        {
+            return Accumulator = Accumulator * multiplier;
+        }
+
+        public double Power(double exp)
+        {
+            if (Accumulator < 0 && exp != Convert.ToInt32(exp))
+            {
+                Accumulator = 0;
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                return Accumulator = Math.Pow(Accumulator, exp);
+            }
+        }
+
+        public double Divide(double divisor)
+        {
+            if (divisor != 0)
+            {
+                return Accumulator = Accumulator / divisor;
+            }
+            else
+            {
+                Accumulator = 0;
+                throw new DivideByZeroException();
+            }
+        }
+
     }
 }
